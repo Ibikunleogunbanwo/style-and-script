e731794e2f0986920cb146d3f92b090993585c17
Ibikunleogunbanwo <ibikunleogunbanwo@gmail.com>
2025-05-22 15:09:59 -0600

    Updated the filter

--- src/app/browser-extension/page.jsx
+++ src/app/browser-extension/page.jsx
@@ -6,10 +6,25 @@ import Image from "next/image";
 import Logo from "@/component/logo";
 import darkicon from "../../assets/images/icon-moon.svg";
 import lighticon from "../../assets/images/icon-sun.svg";
-import Card from "../../component/card"
+import Card from "../../component/card";
+import Carddata from "../data/data.json";
+import { useSearchParams, useRouter } from "next/navigation";
 
 const Browser = () => {
   const [theme, setTheme] = useState("light");
+
+  const router = useRouter();
+
+  const searchParams = useSearchParams();
+  console.log(searchParams.get("filter"));
+
+  const [filterMode, setFilterMode] = useState("all");
+
+  const updateFilter = (filter) => {
+    const params = new URLSearchParams(searchParams);
+    params.set("filter", filter);
+    router.push(`?${params.toString()}`);
+  };
 
   const toggleTheme = () => {
     if (theme === "light") setTheme("dark");
@@ -21,50 +36,87 @@ const Browser = () => {
     else document.documentElement.classList.add("dark");
   }, [theme]);
 
+  useEffect(() => {
+    const filter = searchParams.get("filter");
+    setFilterMode(filter);
+  }, [searchParams]);
+
   return (
-    <main className="mx-3">
-    <div className="h-screen flex flex-col items-center justify-start gap-8 pt-8 px-4">
-      <div className="flex items-center justify-between p-4 w-full lg:w-5/6  shadow-2xl rounded-xl dark:bg-browser-neutral-100 bg-browser-neutral-0">
-        <Logo />
-        <div className="p-2 bg-browser-neutral-200 shadow-lg flex justify-center items-center rounded-xl">
-          <button onClick={toggleTheme}>
-            <Image
-              src={theme === "light" ? darkicon : lighticon}
-              alt="logo for icon to switch mode"
+    <main className="mx-3 my-8">
+      <div className="min-h-screen flex flex-col items-center justify-start gap-8 pt-8 px-4">
+        <div className="flex items-center justify-between p-4 w-full lg:w-5/6  shadow-2xl rounded-xl dark:bg-browser-neutral-100 bg-browser-neutral-0">
+          <Logo />
+          <div className="p-2 bg-browser-neutral-200 shadow-lg flex justify-center items-center rounded-xl">
+            <button onClick={toggleTheme}>
+              <Image
+                src={theme === "light" ? darkicon : lighticon}
+                alt="logo for icon to switch mode"
+              />
+            </button>
+          </div>
+        </div>
+
+        <div className="flex flex-col md:flex-row justify-center md:justify-between w-full lg:w-5/6 items-center text-3xl font-bold text-browser-neutral-900 dark:text-white px-4">
+          <h1 className="text-2xl">Extension List</h1>
+          <div className="flex space-x-2 md:space-x-4 text-xl p-4">
+            <button
+              onClick={() => updateFilter("all")}
+              className={`w-12 md:w-15 h-10 text-base rounded-3xl shadow-2xl text-shadow-zinc-200
+              ${
+                filterMode === "all"
+                  ? "bg-browser-red-400 text-white dark:text-black"
+                  : "bg-browser-neutral-0"
+              }
+              hover:bg-browser-red-400 hover:text-white dark:hover:text-black`}
+            >
+              All
+            </button>
+
+            <button
+              onClick={() => updateFilter("active")}
+              className={`bg-browser-neutral-0 w-17 md:w-20 h-10 text-base rounded-3xl shadow-2xl text-shadow-zinc-200 
+                ${
+                  filterMode === "active"
+                    ? "bg-browser-red-400 text-white dark:text-black"
+                    : "bg-browser-neutral-0"
+                }
+              hover:bg-browser-red-400 hover:text-white dark:hover:text-black`}
+            >
+              Active
+            </button>
+            <button
+              onClick={() => updateFilter("inactive")}
+              className={`bg-browser-neutral-0 w-22 md:w-25 h-10 text-base rounded-3xl shadow-2xl text-shadow-zinc-200 
+                ${
+                  filterMode === "inactive"
+                    ? "bg-browser-red-400 text-white dark:text-black"
+                    : "bg-browser-neutral-0"
+                }
+              
+                hover:bg-browser-red-400 hover:text-white dark:hover:text-black`}
+            >
+              Inactive
+            </button>
+          </div>
+        </div>
+
+        <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-4 w-full lg:w-5/6">
+          {Carddata.filter((card) => {
+            if (filterMode === "active") return card.isActive === true;
+            if (filterMode === "inactive") return card.isActive === false;
+            return true;
+          }).map((card) => (
+            <Card
+              key={card.name}
+              logo={card.logo}
+              name={card.name}
+              description={card.description}
+              isActive={card.isActive}
             />
-          </button>
+          ))}
         </div>
       </div>
-
-      <div className="flex flex-col md:flex-row justify-center md:justify-between w-full lg:w-5/6 items-center text-3xl font-bold text-browser-neutral-900 dark:text-white px-4">
-        <h1 className="text-2xl">Extension List</h1>
-        <div className="flex space-x-2 md:space-x-4 text-xl p-4">
-          <button className="bg-browser-neutral-0 w-12 md:w-15 h-10 text-base rounded-3xl shadow-2xl text-shadow-zinc-200 hover:bg-browser-red-400 hover:text-white dark:hover:text-black">
-            All
-          </button>
-          <button className="bg-browser-neutral-0 w-17 md:w-20 h-10 text-base rounded-3xl shadow-2xl text-shadow-zinc-200 hover:bg-browser-red-400 hover:text-white dark:hover:text-black">
-            Active
-          </button>
-          <button className="bg-browser-neutral-0 w-22 md:w-25 h-10 text-base rounded-3xl shadow-2xl text-shadow-zinc-200 hover:bg-browser-red-400 hover:text-white dark:hover:text-black">
-            Inactive
-          </button>
-        </div>
-      </div>
-
-
-
-     <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-4 w-full lg:w-5/6">
-     <Card isActive = 'true'/>
-     <Card/>
-     <Card/>
-     <Card/>
-     <Card/>
-     </div>
-      
-      <div className="dark:text-white text-browser-neutral-900">{theme}</div>
-    </div>
     </main>
-   
   );
 };
 
--- src/component/card.jsx
+++ src/component/card.jsx
@@ -2,32 +2,33 @@ import React, { useState } from "react";
 import Image from "next/image";
 import deviens from "../assets/images/logo-devlens.svg";
 
-const Card = () => {
+const Card = (data) => {
 
-  const [isActive, setIsActive] = useState(true);
+  const [isActive, setIsActive] = useState(data.isActive);
 
   const handleToggle = () => setIsActive(prev => !prev);
 
   return (
-    <div className="h-40 md:h-40 lg:h-40 bg-browser-neutral-0 dark:bg-browser-neutral-100 border-none rounded-xl shadow-md text-wrap transition-colors duration-300">
-      <div className="flex gap-2 pt-3 pb-2 px-2">
+    <div className="h-45 grid content-between bg-browser-neutral-0 dark:bg-browser-neutral-100 border-none rounded-xl shadow-md text-wrap transition-colors duration-300">
+      <div className="flex gap-2 pt-3 px-2 h-auto items-start">
         <Image className="h-14" 
-        src={deviens} 
+        src={data.logo}
+        width={100}
+        height={100}
         alt="logo_image"
         aria-hidden="true"
-        >
-        </Image>
+        />
         <div className="flex flex-col ">
           <h1 className="font-extrabold text-base dark:text-white mb-1 ">
-            DevLens
+            {data.name}
           </h1>
-          <p className="text-sm dark:text-white text-pretty">
-            Quickly inspect page layouts and visualize element boundaries
+          <p className="text-sm dark:text-white ">
+            {data.description}
           </p>
         </div>
       </div>
 
-      <div className="flex justify-between items-center px-2">
+      <div className="flex flex-nowrap justify-between items-center px-2 h-auto w-full">
         <div className="text-xs m-2">
           <button className=" px-4 py-2 border border-browser-neutral-300 rounded-3xl mb-4 hover:bg-browser-red-500 hover:text-white dark:text-white hover:dark:text-black font-bold">
             Remove
